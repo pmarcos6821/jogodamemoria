e776dd1088e855cbbf5958e48c947de32ee7977c
pmarcos6821 <80109228+pmarcos6821@users.noreply.github.com>
2025-09-22 19:28:54 -0300

    Update registro.py

--- utils/registro.py
+++ utils/registro.py
@@ -1,1 +1,16 @@
+import csv
+import os
 
+ARQUIVO_REGISTRO = "dados_jogadores.csv"
+
+def registrar_jogador(nome):
+    if not os.path.exists(ARQUIVO_REGISTRO):
+        with open(ARQUIVO_REGISTRO, mode='w', newline='') as arquivo:
+            writer = csv.writer(arquivo)
+            writer.writerow(["Nome", "Resultado", "Tempo (s)"])
+    print(f"Bem-vindo, {nome}! Boa sorte no jogo.")
+
+def salvar_resultado(nome, resultado, tempo):
+    with open(ARQUIVO_REGISTRO, mode='a', newline='') as arquivo:
+        writer = csv.writer(arquivo)
+        writer.writerow([nome, resultado, tempo])
